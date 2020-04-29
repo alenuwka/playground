bba0880c8b981be09148de41c059e9bb78ab0105
alenuwka <aljona.skubi@gmail.com>
2020-04-29 20:59:14 +0300

    second task done

--- task.js
+++ task.js
@@ -2,3 +2,9 @@
 //1 
 var person = {"name":"Aljona", "age": 27, "sex": "female"};
 console.log(person);
+
+function getBirthYear(personAge){
+    return 2020 - personAge;
+};
+
+console.log(getBirthYear(person.age));
