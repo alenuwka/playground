ecbb7675efcd53971a5e6b08dbb31cd1b0d49465
alenuwka <aljona.skubi@gmail.com>
2020-04-29 21:19:02 +0300

    3 task done

--- task.js
+++ task.js
@@ -3,8 +3,16 @@
 var person = {"name":"Aljona", "age": 27, "sex": "female"};
 console.log(person);
 
+//2
 function getBirthYear(personAge){
-    return 2020 - personAge;
+    var birthYear = 2020 - personAge;
+    if(birthYear<2000){
+        console.log("Not Millenial");
+    }else{
+        console.log("Millenial");
+    }
+
 };
 
-console.log(getBirthYear(person.age));
+getBirthYear(person.age);
+
